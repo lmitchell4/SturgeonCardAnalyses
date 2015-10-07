4aeb1b48e89572f631ad3446f89febd63b05fa43
Jason DuBois <jason.dubois@wildlife.ca.gov>
2015-10-07 16:36:05 -0700

    Want to query only records for which Card is active (StatusCodeID = 165)

--- sql/PurchasedReportCards.sql
+++ sql/PurchasedReportCards.sql
@@ -3,6 +3,11 @@
 --                from 2013 to 2015; query includes info regarding
 --                which anglers returned cards and for which year
 
+-- added StatusCodeID (07-Oct-2015) to identify active vs inactive
+-- cards - this will help with weeding out records where angler
+-- has > 1 card in a year with only one being active
+-- StatusCodeID = 165 is active, see table dbo.StatusCode
+
 -- declare parameter variables
 DECLARE @year1 AS Integer;
 DECLARE @year2 AS Integer;
@@ -15,7 +20,8 @@ WITH PurchasedCards_CTE
 AS
 (
   SELECT dbo.License.LicenseID, Quantity, dbo.License.ItemID, ItemYear,
-  dbo.License.CustomerID, DocumentID, DateSubmitted, UserID
+  dbo.License.CustomerID, DocumentID, DateSubmitted, UserID,
+  dbo.License.StatusCodeID
   --dbo.LicenseReport.LicenseReportTemplateID
   -- including ItemName, ItemNumber added +1 minute to the
   -- query but did verify records belong to Sturgeon Card
