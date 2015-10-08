e6dfaf89dea83b1ed3a3d7e24cae31f5aae5e98d
Jason DuBois <jason.dubois@wildlife.ca.gov>
2015-10-08 14:16:36 -0700

    Making it easier to select only "Active" cards in the analytics

--- sql/PurchasedReportCards.sql
+++ sql/PurchasedReportCards.sql
@@ -7,6 +7,9 @@
 -- cards - this will help with weeding out records where angler
 -- has > 1 card in a year with only one being active
 -- StatusCodeID = 165 is active, see table dbo.StatusCode
+-- Using Decription field instead of StatusCodeID (08-Oct-2015)
+-- makes for easier subsetting in R using 'Active' instead of trying
+-- to remember the code 165
 
 -- declare parameter variables
 DECLARE @year1 AS Integer;
@@ -21,17 +24,20 @@ AS
 (
   SELECT dbo.License.LicenseID, Quantity, dbo.License.ItemID, ItemYear,
   dbo.License.CustomerID, DocumentID, DateSubmitted, UserID,
-  dbo.License.StatusCodeID
+  dbo.StatusCode.Description AS StatusCodeDesc
   --dbo.LicenseReport.LicenseReportTemplateID
   -- including ItemName, ItemNumber added +1 minute to the
   -- query but did verify records belong to Sturgeon Card
   FROM dbo.Item LEFT JOIN (dbo.License LEFT JOIN dbo.LicenseReport
-    ON dbo.License.LicenseID = dbo.LicenseReport.LicenseID)
+    ON dbo.License.LicenseID = dbo.LicenseReport.LicenseID INNER JOIN
+	  dbo.StatusCode ON dbo.License.StatusCodeID = 
+	  dbo.StatusCode.StatusCodeID)
   ON dbo.Item.ItemID = dbo.License.ItemID
   WHERE RootItemNumberID = 272 AND
         ItemYear BETWEEN @year1 AND @year2
   --ORDER BY CustomerID, ItemYear
 )
+-- get all records from PurchasedCards_CTE (PurchasedCards_CTE.*)
 SELECT PurchasedCards_CTE.*, dbo.CustomerIdentity.IdentityValue,
   dbo.CustomerIdentity.IdentityTypeID, dbo.CustomerIdentity.Status
 FROM PurchasedCards_CTE LEFT JOIN dbo.CustomerIdentity ON
